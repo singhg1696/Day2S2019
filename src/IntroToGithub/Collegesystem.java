66d3c50a76b7d27f0b4c0072264ee14f9d52c202
macstudent <singhg1696>
2019-05-23 18:59:35 -0400

    [Gurwinder] Remove line one

--- src/IntroToGithub/Collegesystem.java
+++ src/IntroToGithub/Collegesystem.java
@@ -3,7 +3,6 @@ package IntroToGithub;
 public class Collegesystem {
     public static void main(String[] args)
     {
-        System.out.println("This is Gurwinder");
         System.out.println("Hello Java");
     }
 }
