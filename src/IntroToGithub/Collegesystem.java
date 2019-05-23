a23fd972ff7413ffe18d3e0307eb003e8c6b27c8
macstudent <singhg1696>
2019-05-23 18:58:43 -0400

    [Gurwinder] College Class added

--- src/IntroToGithub/Collegesystem.java
+++ src/IntroToGithub/Collegesystem.java
@@ -1,7 +1,9 @@
 package IntroToGithub;
 
 public class Collegesystem {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("This is Gurwinder");
         System.out.println("Hello Java");
     }
 }
