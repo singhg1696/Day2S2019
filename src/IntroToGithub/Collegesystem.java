7e53b15a3bfc0c23aed06f321c242dcb03df05e4
macstudent <singhg1696>
2019-05-23 19:20:37 -0400

    [Gurwinder] Name added

--- src/IntroToGithub/Collegesystem.java
+++ src/IntroToGithub/Collegesystem.java
@@ -1,8 +1,13 @@
 package IntroToGithub;
 
-public class Collegesystem {
+public class Collegesystem
+
+{
     public static void main(String[] args)
     {
-        System.out.println("Hello Java");
+        for (int i=1;i<=4;i++)
+        {
+            System.out.println("Gurwinder Singh");
+        }
     }
 }
